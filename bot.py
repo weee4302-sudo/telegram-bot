845b504fffe68b7e200c151c14dad79c229e104b
weee4302-sudo <weee4302@gmail.com>
2026-01-01 17:49:09 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,11 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"OK")
-
+def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+    
     def log_message(self, format, *args):
         return
 
@@ -384,3 +388,4 @@ if __name__ == "__main__":
     application = build_app()
 
     application.run_polling()
+
